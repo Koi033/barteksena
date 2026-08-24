16212cc403e41b7ff39577e865150fdfcee8dcb5
Alitan-rgbx <alanbetancourtbriceno101@gmail.com>
2026-08-24 19:52:04 +0000

    puerto recuperacion

--- config/setting.php
+++ config/setting.php
@@ -3,7 +3,7 @@
 define("HOST", "smtp.gmail.com");
 define("USERNAME", "Bartekappnr@gmail.com");
 define("PASSWORD", "fjsakputoabclqgu");
-
+define("PORT", 587);
 define("SMTP_SECURE", "TLS");
 define("TIEMPO_VIDA", time() + 300);
 //tiempo actual + 5 minutos (300 segundos) para que el token expire
